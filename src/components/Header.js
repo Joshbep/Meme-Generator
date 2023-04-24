5f44e163672f95ede57f794fe4da48215e892741
jbep <joshuabemartin@gmail.com>
2023-04-24 17:23:59 -0400

    started styling the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,13 @@
 function Header() {
     return (
-        <h1>header</h1>
+        <header className="header">
+            <img 
+                src={require("../images/troll-face.png")} 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
     )
 }
 
