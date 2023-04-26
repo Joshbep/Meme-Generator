de28543f763688de4bdcb9d468bb9450545591cc
jbep <joshuabemartin@gmail.com>
2023-04-26 13:14:33 -0400

    imported use state

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import Header from './components/Header'
 import Meme from './components/Meme'
 
 function App() {
+  const [result, func] = useState("Yes")
   return (
     <div className="App">
       <Header />
       <Meme />
+      <><h1>{result}</h1></>
     </div>
   );
 }
